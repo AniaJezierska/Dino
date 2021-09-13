b881eb73aa82118b16e8184d1586b5560041b9c3
Ania <candyrockstore5@gmail.com>
2021-09-13 13:31:44 +0200

    Create constructor functions

---
+++ app.js
@@ -0,0 +1,24 @@
+const compareButton = document.querySelector("#btn");
+const imgUrl = "images/";
+const endpoint = "./dino.json";
+
+function AnimalSpec(species, height, weight, diet) {
+  this.species = species;
+  this.height = height;
+  this.weight = weight;
+  this.diet = diet;
+  this.image = `${imgUrl}${species.toLowerCase()}.png`;
+}
+
+function Dino(species, height, weight, diet, when, where, fact) {
+  Individual.call(this, species, height, weight, diet);
+  this.when = when;
+  this.where = where;
+  this.fact = fact;
+}
+
+function Human(name, height, weight, diet) {
+  Individual.call(this, "human", height, weight, diet);
+  this.name = name;
+  this.diet = diet;
+}
