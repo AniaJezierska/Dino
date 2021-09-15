977c54bd22265c9aee0dda7e166f29f409fc01e1
Ania <candyrockstore5@gmail.com>
2021-09-15 08:24:23 +0200

    Create compare methods

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const compareButton = document.querySelector("#btn");
 const imgUrl = "images/";
 const endpoint = "./dino.json";
+const form = document.querySelector("#dino-compare");
 
 function AnimalSpec(species, height, weight, diet) {
   this.species = species;
@@ -28,3 +29,35 @@ function Human(name, height, weight, diet) {
 
 Human.prototype = Object.create(AnimalSpec.prototype);
 Human.prototype.constructor = Human;
+
+// Create Dino Compare Method 1 - Diet
+const compareDiet = function (humanDiet, dino) {
+  return dino.diet === humanDiet
+    ? `Wow!!! You and ${dino.species} has the same diet!`
+    : `You have so different diet! ${dino.species} is ${dino.diet}`;
+};
+
+// Create Dino Compare Method 2 - Height
+const compareHeight = function (humanHeight, dino) {
+  let diffHeight = dino.height - humanHeight;
+
+  return dino.height > humanHeight
+    ? `The ${dino.species} is ${diffHeight} inches taller than you!`
+    : dino.height < humanHeight
+    ? `Wow!!! You're ${Math.abs(diffHeight)} inches taller than the ${
+        dino.species
+      }!`
+    : `Amazing!!! You're as tall as ${dino.species}`;
+};
+
+// Create Dino Compare Method 3 - Weight
+const compareWeight = function (humanWeight, dino) {
+  let diffWeight = dino.weight - humanWeight;
+  return dino.weight > humanWeight
+    ? `The ${dino.species} is ${diffWeight} lbs heavier than you!`
+    : dino.weight < humanWeight
+    ? `Wow!!! You're ${Math.abs(diffWeight)} lbs heavier than the ${
+        dino.species
+      }!`
+    : `Amazing!!! You're as heavy as ${dino.species}`;
+};
